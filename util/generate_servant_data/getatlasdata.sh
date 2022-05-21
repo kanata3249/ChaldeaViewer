4950cfc14d1f2f3fca4e62b6ecdd231a67c80205
kanata3249 <kanata3249@gmail.com>
2022-05-22 00:18:14 +0900

    update get/generate scripts.

---
+++ util/generate_servant_data/getatlasdata.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+INPUT_DIR=input/
+
+mkdir -p $INPUT_DIR
+
+curl -o $INPUT_DIR/0_atlas.json 'https://api.atlasacademy.io/export/JP/nice_servant.json'
+curl -o $INPUT_DIR/6_bgm.json 'https://api.atlasacademy.io/export/JP/nice_bgm.json'
+curl -o $INPUT_DIR/9_bondequip.json https://api.atlasacademy.io/export/JP/nice_equip.json
--- util/generate_servant_data/getdata.sh
+++ util/generate_servant_data/getdata.sh
@@ -4,10 +4,8 @@ INPUT_DIR=input/
 
 mkdir -p $INPUT_DIR
 
-curl -o $INPUT_DIR/0_atlas.json 'https://api.atlasacademy.io/export/JP/nice_servant.json'
 curl -o $INPUT_DIR/1_servants.csv 'https://docs.google.com/spreadsheets/d/1KwwztukPMpT8KzdFxQsmm2gbCaRQqE-7/gviz/tq?gid=1498357704&tqx=out:csv'
 curl -o $INPUT_DIR/2_skills.csv 'https://docs.google.com/spreadsheets/d/1KwwztukPMpT8KzdFxQsmm2gbCaRQqE-7/gviz/tq?gid=1242219468&tqx=out:csv'
 curl -o $INPUT_DIR/3_items.csv 'https://docs.google.com/spreadsheets/d/1KwwztukPMpT8KzdFxQsmm2gbCaRQqE-7/gviz/tq?gid=1300403258&tqx=out:csv'
 curl -o $INPUT_DIR/4_appendSkill.csv 'https://docs.google.com/spreadsheets/d/1KwwztukPMpT8KzdFxQsmm2gbCaRQqE-7/gviz/tq?gid=387834776&tqx=out:csv'
 curl -o $INPUT_DIR/5_costume.csv 'https://docs.google.com/spreadsheets/d/1KwwztukPMpT8KzdFxQsmm2gbCaRQqE-7/gviz/tq?gid=675248781&tqx=out:csv'
-curl -o $INPUT_DIR/6_bgm.json 'https://api.atlasacademy.io/export/JP/nice_bgm.json'
